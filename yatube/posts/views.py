fc4f004cd5632f9142ff20b9136a1123b5f3bf38
Арсен Григорян <arsen8577711@yandex.ru>
2022-08-25 21:53:05 +0300

    Загружаю

---
+++ yatube/posts/views.py
@@ -0,0 +1,122 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+from django.db.models import Count
+from .models import Post, Group, User
+from .form import PostForm
+
+
+def index(request):
+    main = "Последние обновления на сайте"
+    post_list = Post.objects.order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+        'main': main
+    }
+    template = 'posts/index.html'
+    return render(request, template, context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+        'group': group,
+        'posts': posts,
+    }
+    template = 'posts/group_list.html'
+    return render(request, template, context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    posts = Post.objects.filter(author=author).order_by('-pub_date')
+    count = Post.objects.filter(author=author).aggregate(Count('pk'))
+    posts_other = Post.objects.exclude(author=author)
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+        'posts_other': posts_other,
+        'count': count,
+        'author': author,
+        'posts': posts
+    }
+    template = 'posts/profile.html'
+    return render(request, template, context)
+
+
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    count = Post.objects.filter(author_id=post.author_id).aggregate(
+        Count('pk')
+    )
+    context = {
+        'count': count,
+        'post': post,
+    }
+    template = 'posts/post_detail.html'
+    return render(request, template, context)
+
+
+def post_create(request):
+    main = 'Создать пост'
+    is_edit = False
+    group = Group.objects.all()
+    title = request.user.username
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return HttpResponseRedirect(f'/profile/{title}')
+    else:
+        form = PostForm()
+
+    context = {
+        'main': main,
+        'is_edit': is_edit,
+        'title': title,
+        'group': group,
+        'form': form
+    }
+    template = 'posts/create_post.html'
+    return render(request, template, context)
+
+
+def post_edit(request, post_id):
+    group = Group.objects.all()
+    post = get_object_or_404(Post, pk=post_id)
+    form = PostForm(request.POST, instance=post)
+    main = 'Редактировать пост'
+    is_edit = True
+    if request.user.id == post.author_id:
+        if request.method == 'POST':
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.save()
+                return HttpResponseRedirect(f'/posts/{post_id}')
+            else:
+                form = PostForm(instance=post)
+    else:
+        return HttpResponseRedirect(f'/posts/{post_id}')
+    context = {
+        'group': group,
+        'main': main,
+        'post': post,
+        'form': form,
+        'is_edit': is_edit,
+    }
+    template = 'posts/create_post.html'
+    return render(request, template, context)
+
